a9543241969464fbb54222cf6a9b3fd62411d855
sudobangbang <gregcorradini@gmail.com>
2015-05-26 07:01:00 -0700

    finalize descriptors part

--- Examples/week-04-oo/magic_methods/name_descriptor.py
+++ Examples/week-04-oo/magic_methods/name_descriptor.py
@@ -17,3 +17,16 @@ class Name(object):
     def __delete__(self,obj):
         print 'DELETE:'
         self.value = None
+
+class User(object):
+
+    def __init__(self,name_str):
+        name = Name(name_str)
+
+if __name__ == '__main__':
+    u = User('bob')
+    print u.name
+    u.name = 'sarah'
+    print u.name
+    del u.name
+    
